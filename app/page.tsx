37bd49dac014e413891d6cb970c8f53185c0383b
lgaieta <lgaieta.dev@gmail.com>
2023-07-01 20:21:32 -0300

    Added title and subtitle to home

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import Image from 'next/image'
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    </main>
-  )
+    return (
+        <main className="flex flex-col gap-6 items-center pt-6 text-dark px-6">
+            <h1 className='text-3xl font-bold text-center'>Probá nuestra <em className='text-accent not-italic'>inteligencia artificial</em></h1>
+            <p className='text-base text-center'>Apuntá a un producto y averiguá si es reciclable o no.</p>
+        </main>
+    )
 }
