a418a07f60172fc8620acf63baf7e42c987bd128
lgaieta <lgaieta.dev@gmail.com>
2023-07-01 20:21:13 -0300

    Added colors to theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,13 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      colors: {
+        dark: "#101010",
+        light: "#FCFFFB",
+        border: "#DFDFDF",
+        accent: "#12BD0F",
+        "accent-dark": "#11830F"
+      }
     },
   },
   plugins: [],
