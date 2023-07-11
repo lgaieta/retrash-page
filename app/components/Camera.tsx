4102921a0247f6462b52d027bd850362a5338288
lgaieta <lgaieta.dev@gmail.com>
2023-07-10 23:46:36 -0300

    Added some tfjs things

--- app/components/Camera.tsx
+++ app/components/Camera.tsx
@@ -1,11 +1,23 @@
 "use client";
 import { useRef } from "react";
 import Webcam from "react-webcam";
+import * as tf from "@tensorflow/tfjs";
 
 function Camera() {
     const cameraRef = useRef(null);
+    const testModel = async () => {
+        const model = await tf.loadLayersModel("/api");
+        console.log(model.summary);
+    };
 
-    return <Webcam audio={false} ref={cameraRef} onUserMedia={() => setTimeout(() => console.log(cameraRef.current.getScreenshot()), 1500)} screenshotFormat="image/jpeg" />
+    return (
+        <Webcam
+            audio={false}
+            ref={cameraRef}
+            onUserMedia={() => setTimeout(() => testModel(), 1500)}
+            screenshotFormat="image/jpeg"
+        />
+    );
 }
 
-export default Camera
+export default Camera;
